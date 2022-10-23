39357b17ac735d1f2faee96b7e480f14a71a2344
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2022-10-23 22:19:02 +0900

    モデルで日付ごとに学習時間をグループ化

--- src/app/Base.php
+++ src/app/Base.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Base extends Model
 {
@@ -14,5 +15,14 @@ class Base extends Model
     public function languages()
     {
         return $this->hasMany("App\Languages");
+    }
+
+    public function fetDateTime()
+    {
+        return DB::table('bases')
+                ->selectRaw('DATE_FORMAT(created_at, "%Y%m%d") AS date')
+                ->selectRaw('SUM(time) AS day_time')
+                ->groupBy('date')
+                ->get();
     }
 }
